690aff603b65129b20ce3a5585ee6157a57fe752
Araujo <86306571+PhelipeCurso@users.noreply.github.com>
2023-07-14 17:49:32 -0300

    Atualizacao do projeto docentes incluido

--- PWS/Models/Docentes.cs
+++ PWS/Models/Docentes.cs
@@ -3,47 +3,45 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PWS.Models
 {
+    [Table("Docentes")]
     public class Docentes
     {
-        [Table("Docentes")]
-        public class Alunos
-        {
             public int Id { get; set; }
 
             [Required]
-            public string Nome { get; set; }
+            public string? Nome { get; set; }
 
             public int Idade { get; set; }
 
             [EmailAddress]
-            public string Email { get; set; }
+            public string? Email { get; set; }
 
             [Column("cpf")]
-            public string CPF { get; set; }
+            public string? CPF { get; set; }
 
             [Column("cnpj")]
-            public string CNPJ { get; set; }
+            public string? CNPJ { get; set; }
 
             [Column("cep")]
-            public string CEP { get; set; }
+            public string? CEP { get; set; }
 
             [Column("endereco")]
-            public string Endereco { get; set; }
+            public string? Endereco { get; set; }
 
             [Column("cidade")]
-            public string Cidade { get; set; }
+            public string? Cidade { get; set; }
 
             [Column("bairro")]
-            public string Bairro { get; set; }
+            public string? Bairro { get; set; }
 
             [Column("complemento")]
-            public string Complemento { get; set; }
+            public string? Complemento { get; set; }
 
             [Column("telefone")]
-            public string Telefone { get; set; }
+            public string? Telefone { get; set; }
 
             [Column("celular")]
-            public string Celular { get; set; }
+            public string? Celular { get; set; }
 
             [Column("ativo")]
             public bool Ativo { get; set; }
@@ -52,7 +50,7 @@ namespace PWS.Models
             public DateTime Data_do_Cadastro { get; set; }
 
             [Column("observacao")]
-            public string Observacao { get; set; }
-        }
+            public string? Observacao { get; set; }
+        
     }
 }
